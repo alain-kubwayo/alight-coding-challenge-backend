b96ab39fd4558f5a171ee95ead1167a8a35f9b37
Alain Kubwayo <ahkubwayo@gmail.com>
2024-07-11 20:41:36 +0200

    feat: rename docs query

--- src/controllers/search.ts
+++ src/controllers/search.ts
@@ -2,7 +2,7 @@ import { RequestHandler } from "express";
 import { searchService } from "../services/search";
 
 export const searchController: RequestHandler = async (req, res) => {
-  const searchQuery = req.query.query as string;
+  const searchQuery = req.query.q as string;
   try {
     const results = await searchService(searchQuery);
     return res.status(200).json(results);
